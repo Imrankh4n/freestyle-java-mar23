14ed4f330e2f191f719ed577485c02ad02a61d76
Imrankh4n <127673321+Imrankh4n@users.noreply.github.com>
2023-03-19 15:33:21 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
    public static void main(String[] args) {
 
       System.out.println("Hello, world!");
-      System.out.println("Another line for Peridically Build");
+      System.out.println("Another line for Poll SCM");
 
    }
 
